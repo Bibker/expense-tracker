4b361ff77737448a8e6bfbdece92b3eeb9212a20
Aliza Regmi <iamalizaer@gmail.com>
2024-04-30 13:27:06 +0545

    Backend Deployed URL Updated

--- frontend/src/Context/globalContext.js
+++ frontend/src/Context/globalContext.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import swal from "sweetalert";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://expense-tracker-backend-peach.vercel.app";
 
 const GlobalContext = React.createContext();
 
